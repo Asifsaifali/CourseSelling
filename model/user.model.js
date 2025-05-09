be8fc609d872b0f91ed11eb77627a80c64616883
Asifsaifali <asif263848@gmail.com>
2025-05-09 23:15:29 +0530

    configuared the dotenv file and created user Schema

---
+++ model/user.model.js
@@ -0,0 +1,38 @@
+import mongoose, { mongo } from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    verificationToken : {
+        type: String,
+    },
+    resetPasswordExpiry : {
+        type : Date,
+    }
+
+  },
+  { timestamps: true }
+);
