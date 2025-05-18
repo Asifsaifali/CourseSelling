6091c07c6b19489af744085a413aee8ddd8404a4
Asifsaifali <asif263848@gmail.com>
2025-05-18 19:33:36 +0530

    added login routes and fetched user by email

--- routes/users/user.routes.js
+++ routes/users/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser, loginUser } from "../../controller/user.controller.js";
+import { registerUser, loginUser, getUserByEmail } from "../../controller/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/login",loginUser)
+router.get("/user", getUserByEmail);
 
 export default router;
