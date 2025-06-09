b0695f520be3ec66632afb89100da4e759210f62
Asifsaifali <asif263848@gmail.com>
2025-06-09 18:17:59 +0530

    added admin middleware in the course creation routes

--- routes/courses/courses.routes.js
+++ routes/courses/courses.routes.js
@@ -1,10 +1,10 @@
 import { createCourse } from "../../controller/course.controller.js";
-import { authenticate, authorizeAdmin } from "./../../middleware/auth.js"
+import adminMiddleware from "../../middleware/adminMiddleware.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/create", authenticate, authorizeAdmin, createCourse)
+router.post("/create",adminMiddleware, createCourse)
 
 
 export default router;
