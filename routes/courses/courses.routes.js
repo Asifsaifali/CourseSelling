808cd9d8aa35fa9445476c7899e3d800e2ee5582
Asifsaifali <asif263848@gmail.com>
2025-06-13 11:22:29 +0530

    course routes to post course purchase

--- routes/courses/courses.routes.js
+++ routes/courses/courses.routes.js
@@ -1,10 +1,11 @@
-import { createCourse } from "../../controller/course.controller.js";
+import { createCourse, purchaseCourse } from "../../controller/course.controller.js";
 import adminMiddleware from "../../middleware/adminMiddleware.js";
 import express from "express";
+import { userMiddleware } from "../../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/create",adminMiddleware, createCourse)
-
+router.post("/purchase",userMiddleware ,purchaseCourse);
 
 export default router;
