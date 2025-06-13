528572ff4c627bd11fd40d32f21a657cb0d780ad
Asifsaifali <asif263848@gmail.com>
2025-06-13 12:13:52 +0530

    View all courses repo

--- repository/user.repository.js
+++ repository/user.repository.js
@@ -51,6 +51,18 @@ class userRepository{
             
         }
     }
+
+    async viewAllCourses(userId){
+        try {
+            const user = await User.findOne({_id :userId})
+            return user;
+        } catch (error) {
+            console.log("Have some error in user repository");
+            console.log(error);
+            throw new Error("Internal server error");
+            
+        }
+    }
 }
 
 export default userRepository;
